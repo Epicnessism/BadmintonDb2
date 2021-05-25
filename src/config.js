c8e648de4c8f27269098f95f6d27789c0705a135
Tony Wang <tonywang0697@yahoo.com>
2021-05-25 02:09:04 -0400

    found issue, typo in localdev

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ var envName = process.argv[2]
 if(envName) {
     console.log('Environment: ' + envName);
 } else {
-    console.log('No environment specified, defaulting to ' + 'localdev');
-    envName = 'localDev'
+    envName = 'localdev'
+    console.log('No environment specified, defaulting to ' + envName);
+    
 }
 var envConfig = require('./config/' + envName + '.js');
 var defaultConfig = require('./config/default.js');
