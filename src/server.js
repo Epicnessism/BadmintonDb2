09735af48157c0e0e091853bbc06397715a810d7
Tony Wang <tonywang0697@yahoo.com>
2021-05-27 02:29:50 -0400

    attempting to log env and port

--- src/server.js
+++ src/server.js
@@ -139,4 +139,6 @@ api.use(function(err, req, res, next) {
 
 
 api.listen(process.env.PORT)
+console.log(process.env);
+console.log(process.env.PORT);
 console.log('Listening for WS and HTTP traffic on port ' + process.env.PORT);
