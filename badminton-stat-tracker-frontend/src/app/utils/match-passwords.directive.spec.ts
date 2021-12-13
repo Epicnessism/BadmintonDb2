6717dd326ac4afdf1f31b5fda9be73e50a6a701b
Tony Wang <tonywang0697@yahoo.com>
2021-12-13 03:39:48 -0500

    removed old angular build, added new working build with bracket

--- badminton-stat-tracker-frontend/src/app/utils/match-passwords.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MatchPasswordsDirective } from './match-passwords.directive';
-
-describe('MatchPasswordsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MatchPasswordsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- badminton-stat-tracker-frontend/src/app/utils/match-passwords.directive.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-
-export const MatchPasswordsValidator: ValidatorFn = (control: AbstractControl) :
-ValidationErrors | null => {
-  const password = control.get('password');
-  const confirmPassword = control.get('confirmPassword');
-
-  return password && confirmPassword && password.value === confirmPassword.value ? {passwordMistmatch: true} : null
-}
