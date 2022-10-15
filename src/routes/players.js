0fa7253458acfc2753a0a963b42058c8589c59b7
Tony Wang <tonywang0697@yahoo.com>
2022-10-15 00:32:24 -0400

    started basics of profile page and added sql query for basic player info

--- src/routes/players.js
+++ src/routes/players.js
@@ -4,7 +4,7 @@ const knex = require('../dbconfig');
 
 
 players.get('/:player_identifier', function(req, res, next) {
-    console.log(req.params.player_identifier);
+    console.log(req.params.player_identifier)
     //do validation logic here
     knex('users')
     .select("*")
@@ -12,13 +12,13 @@ players.get('/:player_identifier', function(req, res, next) {
     .where('users.given_name', 'ilike', `%${req.params.player_identifier}%`)
     .orWhere('users.family_name', 'ilike', `%${req.params.player_identifier}%`)
     .then( result => {
-        console.log(result);
-        res.status(200).json(result);
+        console.log(result)
+        res.status(200).json(result)
     })
 })
 
 players.get('/autocomplete/:player_identifier', function(req, res, next) {
-    console.log(req.params.player_identifier);
+    console.log(req.params.player_identifier)
     //do validation logic here
     knex('users')
     .select('user_id as userId',
@@ -29,9 +29,49 @@ players.get('/autocomplete/:player_identifier', function(req, res, next) {
     .where('users.given_name', 'ilike', `%${req.params.player_identifier}%`)
     .orWhere('users.family_name', 'ilike', `%${req.params.player_identifier}%`)
     .then( result => {
-        console.log(result);
-        res.status(200).json(result);
+        console.log(result)
+        res.status(200).json(result)
     })
+})
+
+/**
+ * * returns the single json object form of a single user found
+ */
+players.get('/:player_identifier/profileData', async function(req, res, next) {
+    console.log(req.params.player_identifier)
+
+    const response = await knex('users as u')
+    .select(
+        'u.user_id as userId',
+        'u.username as username',
+        'u.given_name as givenName',
+        'u.family_name as familyName',
+        'u.email as email',
+        'u.birthday as birthday',
+        'p.level as level',
+        'p.hand as hand',
+        'p.location as location',
+        'p.institution as institution'
+    )
+    .leftJoin('players as p', 'p.player_id', 'u.user_id')
+    .where('u.user_id', req.params.player_identifier)
+    .then( results => {
+        console.log(results)
+
+    })
+    .catch( err => {
+        console.log(err)
+        return err
+    })
+
+    if(response.code != undefined) {
+        next(err)
+        return
+    }
+
+    res.status(200).json(response[0])
+
+
 })
 
 module.exports.players = players;
